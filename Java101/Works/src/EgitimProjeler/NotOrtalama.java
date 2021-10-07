7a8f99ac98f2aeed88131b58c8ba93c6d8136583
Capsalt <hbasri2304@gmail.com>
2021-10-07 12:56:52 +0300

    test

--- Java101/Works/src/EgitimProjeler/NotOrtalama.java
+++ Java101/Works/src/EgitimProjeler/NotOrtalama.java
@@ -1,7 +1,10 @@
 package EgitimProjeler;
 
+import java.util.Scanner;
+
 public class NotOrtalama {
     public static void main(String[] args) {
-        
+
+        Scanner scan = new Scanner(System.in); 
     }
 }
